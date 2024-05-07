185fe135a8a4977263df8747f608a0fed9bbfa2d
Video <videogamesm12@gmail.com>
2024-05-06 21:04:00 -0600

    Implements economy subproject

---
+++ Capitalism/src/main/java/creativitium/revolution/capitalism/Capitalism.java
@@ -0,0 +1,44 @@
+package creativitium.revolution.capitalism;
+
+import creativitium.revolution.capitalism.commands.Command_balance;
+import creativitium.revolution.capitalism.commands.Command_eco;
+import creativitium.revolution.capitalism.commands.Command_pay;
+import creativitium.revolution.capitalism.data.CPlayerService;
+import creativitium.revolution.foundation.Foundation;
+import lombok.Getter;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Capitalism extends JavaPlugin
+{
+    @Getter
+    private static Capitalism instance;
+    //--
+    private CPlayerService playerService;
+
+    @Override
+    public void onLoad()
+    {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable()
+    {
+        // Import our messages
+        Foundation.getInstance().getMessageService().importFrom(this);
+
+        // Register our services
+        playerService = new CPlayerService();
+        Foundation.getInstance().getPlayerDataService().addExternalPlayerService(this,playerService);
+        Bukkit.getServicesManager().register(Economy.class, playerService.getEconomy(), this, ServicePriority.Highest);
+
+        // Register our commands
+        Foundation.getInstance().getCommandLoader().loadCommandsManually("capitalism",
+                new Command_balance(),
+                new Command_eco(),
+                new Command_pay());
+    }
+}
